8366196fae68d5ca07c8dc39d3538cfbeba9e173
samnjuno <samuelnjuno51@gmail.com>
2024-08-21 21:09:17 +0300

    added handler action on the button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
+import { useState } from "react";
 
 
 function App() {
   //javascript logic
+  //create a state
+  const [message, setMessage] = useState('Hello World');
 
+  const handleClick = () => {
+    setMessage('Clicked');
+  };
+
+ 
 
 //render jsx(html and css)
 return(
   <div>
-    <h1>Hellow World</h1>
+    <h1>{message}</h1>
+    <button onClick={handleClick}>Press Me</button>
   </div>
 );
 
